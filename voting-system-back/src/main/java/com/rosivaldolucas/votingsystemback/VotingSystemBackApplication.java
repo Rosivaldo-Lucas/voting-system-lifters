93879e1cd5f7a6e84859c143558b93e2edae50f5
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2024-03-27 17:44:43 -0300

    (backend): Commit inicial. Cria projeto com a versao Java 21 LTS, adiciona dependencias do projeto backend. #1

---
+++ voting-system-back/src/main/java/com/rosivaldolucas/votingsystemback/VotingSystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.rosivaldolucas.votingsystemback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSystemBackApplication.class, args);
+	}
+
+}
---
+++ voting-system-back/src/test/java/com/rosivaldolucas/votingsystemback/VotingSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosivaldolucas.votingsystemback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystemBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
